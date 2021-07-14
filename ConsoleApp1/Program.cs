f91209f1accb8fc43af75b7a29ee0a500d4a70f8
ilkaykabas <73221029+ilkaykabas@users.noreply.github.com>
2021-07-14 11:36:19 +0300

    initialize

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ConsoleApp1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+    
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private const int NumOfIterations = 1;
+
+        [Benchmark]
+        public int IList()
+        {
+            int sum = 0;
+            IList<int> numbers = new List<int> { 0 };
+
+            for (int i = 0; i < NumOfIterations; i++)
+            {
+                foreach (var number in numbers)
+                {
+                    sum += number;
+                }
+            }
+            return sum;
+        }
+
+        [Benchmark]
+        public int List()
+        {
+            int sum = 0;
+            List<int> numbers = new List<int> { 0 };
+
+            for (int i = 0; i < NumOfIterations; i++)
+            {
+                foreach (var number in numbers)
+                {
+                    sum += number;
+                }
+            }
+            return sum;
+        }
+    }
+}
